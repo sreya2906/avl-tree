b69034f0780b5722715689cd617a7dbb81b5562e
Sreya Bhattacharya <sreya2906@gmail.com>
2022-04-08 13:18:17 -0500

    Initial Committ

---
+++ AVL-Tree/src/main/java/PAS/AVL_Tree/App.java
@@ -0,0 +1,13 @@
+package PAS.AVL_Tree;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
