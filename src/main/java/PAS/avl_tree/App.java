24f621c07ec1041cee9cec0c94073ce4fe46e84c
Sreya Bhattacharya <sreya2906@gmail.com>
2022-04-08 20:53:16 -0500

    Add Sample Code Files

---
+++ src/main/java/PAS/avl_tree/App.java
@@ -0,0 +1,13 @@
+package PAS.avl_tree;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
